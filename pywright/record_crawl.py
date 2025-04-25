07ef1f2c03518c9cf91906a694c62120171419f3
dlsdud9098 <dlsdud9098@naver.com>
2025-04-25 15:49:44 +0900

    asdf

--- pywright/record_crawl.py
+++ pywright/record_crawl.py
@@ -6,6 +6,7 @@ import random
 from datetime import datetime, timedelta
 from itertools import chain
 from io import StringIO
+from tabulate import tabulate
 
 defense_position = {
     '중견수': ' 중견수',
@@ -57,81 +58,103 @@ def split_url(urls, split_num):
 
     return new_urls
 
+def get_table(page):
+    pass
+
     
 async def fetch_table(playwright, idx, url):
-    browser = await playwright.chromium.launch(headless=False)
+    browser = await playwright.chromium.launch(headless=True)
     context = await browser.new_context()
     page = await context.new_page()
     await page.goto(url)
     
+    print(url)
+    team1 = await page.locator('#content > div > div.Home_main_section__y9jR4 > section.Home_game_panel__97L_8 > div.Home_game_contents__35IMT > div > div.PlayerRecord_comp_player_record__1tI5G.type_kbo > div.PlayerRecord_tab_area__1oYK_ > div > button:nth-child(1) > strong').inner_text()
+    team2 = await page.locator('#content > div > div.Home_main_section__y9jR4 > section.Home_game_panel__97L_8 > div.Home_game_contents__35IMT > div > div.PlayerRecord_comp_player_record__1tI5G.type_kbo > div.PlayerRecord_tab_area__1oYK_ > div > button:nth-child(2) > strong').inner_text()
+    
+    print(team1, team2)
+    
     # selector 선택
-    first_team_element = await page.locator("#content > div > div.Home_main_section__y9jR4 > section.Home_game_panel__97L_8 > div.Home_game_contents__35IMT > div > div.PlayerRecord_comp_player_record__1tI5G.type_kbo > div.PlayerRecord_tab_area__1oYK_ > div > button:nth-child(1)").click()
-    
-    global defense_position
-    # 테이블 가져오기
-    # table = await page.locator('#content > div > div.Home_main_section__y9jR4 > section.Home_game_panel__97L_8 > div.Home_game_contents__35IMT > div > div.PlayerRecord_comp_player_record__1tI5G.type_kbo > div.PlayerRecord_tabpanel__3GYt9 > div:nth-child(1)').inner_html()
-    # # 데이터 가져오기
-    # df1 = pd.read_html(StringIO(table))[0]
-    
-    # # 컬럼에서 내림차순 변경 지우기
-    # original_columns = list(df1.columns)
-    # new_columns = [column.replace('내림차순 정렬', '')for column in original_columns]
-    # df1.columns = new_columns
-    
-    # df1['교체여부'] = 0
-    
-    # for index, row in df1.iterrows():
-    #     batter = df1.at[index, '타자명'].strip().split('번 타자')
-    #     # 타순 열 분리하기
-    #     if len(batter) > 1:
-    #         batting_order, batter_name = batter
-    #         df1.at[index,'타순'] = batting_order+'번'
-    #         # df.at[index, '타자명'] = batter_name
-            
-    #         # 교체 여부
-    #         if '교체' in batter_name:
-    #             batter_name = batter_name.replace('교체', '')
-    #             df1.at[index, '교체여부'] = 1
-            
-    #         # 수비 위치
-    #         for key, value in defense_position.items():
-    #             if key in batter_name:
-    #                 batter_name = batter_name.replace(key, value)
-    #                 break
-    #         batter_name, defense = batter_name.split(' ')
-            
-    #         df1.at[index, '수비위치'] = defense
-    #         df1.at[index, '타자명'] = batter_name
+    for idx in range(1, 3):
+        if idx == 1:
+            my_team = team1
+            enermy_team = team2
+        first_team_element = await page.locator("#content > div > div.Home_main_section__y9jR4 > section.Home_game_panel__97L_8 > div.Home_game_contents__35IMT > div > div.PlayerRecord_comp_player_record__1tI5G.type_kbo > div.PlayerRecord_tab_area__1oYK_ > div > button:nth-child(1)").click()
         
-    # away_team = await page.locator('#content > div > div.Home_main_section__y9jR4 > section.Home_game_panel__97L_8 > div.Home_game_contents__35IMT > div > div.PlayerRecord_comp_player_record__1tI5G.type_kbo > div.PlayerRecord_tab_area__1oYK_ > div > button:nth-child(2) > strong').text_content()
-    #     # print(aa)
-    # df1['상대팀'] = away_team
-            
-    # df1 = df1[['타순','타자명','상대팀','교체여부','수비위치','타수','득점','안타','타점','홈런','볼넷','삼진','타율','1','2','3','4','5','6','7','8','9']]
+        global defense_position
+        # 테이블 가져오기
+        # table = await page.locator('#content > div > div.Home_main_section__y9jR4 > section.Home_game_panel__97L_8 > div.Home_game_contents__35IMT > div > div.PlayerRecord_comp_player_record__1tI5G.type_kbo > div.PlayerRecord_tabpanel__3GYt9 > div:nth-child(1)').inner_html()
+        # # 데이터 가져오기
+        # df1 = pd.read_html(StringIO(table))[0]
         
-    # # null 값 채우기
-    # df1.fillna('-', inplace=True)
-    
-    
-    
-    # 테이블 가져오기
-    table = await page.locator('#content > div > div.Home_main_section__y9jR4 > section.Home_game_panel__97L_8 > div.Home_game_contents__35IMT > div > div.PlayerRecord_comp_player_record__1tI5G.type_kbo > div.PlayerRecord_tabpanel__3GYt9 > div:nth-child(2)').inner_html()
-    # 데이터 가져오기
-    df2 = pd.read_html(StringIO(table))[0]
-    
-    # 컬럼에서 내림차순 변경 지우기
-    original_columns = list(df2.columns)
-    new_columns = [column.replace('내림차순 정렬', '')for column in original_columns]
-    df2.columns = new_columns
-    
-    
-    
-    print(df2)
-    # print(df)
-    # df1.to_csv('sadf.csv', encoding='utf-8', index=False)
-    # second_team_element = await page.locator("#content > div > div.Home_main_section__y9jR4 > section.Home_game_panel__97L_8 > div.Home_game_contents__35IMT > div > div.PlayerRecord_comp_player_record__1tI5G.type_kbo > div.PlayerRecord_tab_area__1oYK_ > div > button:nth-child(2)").click()
-    
-    # time.sleep(100)
+        # # 컬럼에서 내림차순 변경 지우기
+        # original_columns = list(df1.columns)
+        # new_columns = [column.replace('내림차순 정렬', '')for column in original_columns]
+        # df1.columns = new_columns
+        
+        # df1['교체여부'] = 0
+        
+        # for index, row in df1.iterrows():
+        #     batter = df1.at[index, '타자명'].strip().split('번 타자')
+        #     # 타순 열 분리하기
+        #     if len(batter) > 1:
+        #         batting_order, batter_name = batter
+        #         df1.at[index,'타순'] = batting_order+'번'
+        #         # df.at[index, '타자명'] = batter_name
+                
+        #         # 교체 여부
+        #         if '교체' in batter_name:
+        #             batter_name = batter_name.replace('교체', '')
+        #             df1.at[index, '교체여부'] = 1
+                
+        #         # 수비 위치
+        #         for key, value in defense_position.items():
+        #             if key in batter_name:
+        #                 batter_name = batter_name.replace(key, value)
+        #                 break
+        #         batter_name, defense = batter_name.split(' ')
+                
+        #         df1.at[index, '수비위치'] = defense
+        #         df1.at[index, '타자명'] = batter_name
+            
+        #     # print(aa)
+        # df1['상대팀'] = enermy_team
+
+        # 열 순서 변경
+        # df1 = df1[['타순','타자명','상대팀','교체여부','수비위치','타수','득점','안타','타점','홈런','볼넷','삼진','타율','1','2','3','4','5','6','7','8','9']]
+            
+        # # null 값 채우기
+        # df1.fillna('-', inplace=True)
+        
+        
+        
+        # 테이블 가져오기
+        table = await page.locator('#content > div > div.Home_main_section__y9jR4 > section.Home_game_panel__97L_8 > div.Home_game_contents__35IMT > div > div.PlayerRecord_comp_player_record__1tI5G.type_kbo > div.PlayerRecord_tabpanel__3GYt9 > div:nth-child(2)').inner_html()
+        # 데이터 가져오기
+        df2 = pd.read_html(StringIO(table))[0]
+        
+        # 컬럼에서 내림차순 변경 지우기
+        original_columns = list(df2.columns)
+        new_columns = [column.replace('내림차순 정렬', '')for column in original_columns]
+        df2.columns = new_columns
+        #print(df2.columns)
+        
+        for index, row in df2.iterrows():
+            text = df2.at[index, '이닝']
+            if '⅓' in text:
+                df2.at[index, '이닝'] = text.replace('⅓', '1/3')
+            elif '⅔' in text:
+                df2.at[index, '이닝'] = text.replace('⅔', '2/3')
+                
+        df2['상대팀'] = enermy_team
+        
+        df2.fillna('-', inplace=True)
+        print(tabulate(df2, tablefmt='grid', headers=df2.columns))
+        # print(df)
+        # df1.to_csv('sadf.csv', encoding='utf-8', index=False)
+        # second_team_element = await page.locator("#content > div > div.Home_main_section__y9jR4 > section.Home_game_panel__97L_8 > div.Home_game_contents__35IMT > div > div.PlayerRecord_comp_player_record__1tI5G.type_kbo > div.PlayerRecord_tab_area__1oYK_ > div > button:nth-child(2)").click()
+        
+        # time.sleep(100)
     
 
 async def main():
